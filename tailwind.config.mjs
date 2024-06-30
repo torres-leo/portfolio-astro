cab6a12cac99f6cf2b33f5b0bfa3e07fd984a63b
torres-leo <leojts24@gmail.com>
2024-06-30 01:20:10 -0600

    update portfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "pulse-slow": "pulse 5s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
